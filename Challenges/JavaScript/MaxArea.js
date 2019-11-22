8c8298403fb286e6d78a1fe87daa6e0e05ab10da
pedroOlivarez <pedro.olivarezjr@gmail.com>
2019-11-22 13:30:10 -0600

    Remove console.logs and such

--- Challenges/JavaScript/MaxArea.js
+++ Challenges/JavaScript/MaxArea.js
@@ -5,29 +5,21 @@
 
 const maxArea = height => {
    let maxValue = 0;
-   let iterations = 0;
    height.forEach((element, index) => {
       const minDistance = (maxValue ? Math.floor(maxValue / element) : 0) + index;
       for (let i = minDistance; i < height.length; i++) {
          if (index !== i) {
-            iterations++;
             let area = Math.abs(index - i) * Math.min(element, height[i]);
             maxValue = Math.max(maxValue, area);
          }
       }
    });
-   console.log(iterations);
    return maxValue;
 };
 
-const getRandomArray = num => {
-   const array = [];
-   for (let i = 0; i < num; i++) array.push(Math.floor(Math.random() * 9) + 1);
-   return array;
-};
-
 const input = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+const expectedOutput = 49;
+const output = maxArea(input);
 
-const output = _maxArea(input);
-
-console.log(output);
+console.log(`Expected output: ${expectedOutput}`);
+console.log(`My output:       ${output}`);
