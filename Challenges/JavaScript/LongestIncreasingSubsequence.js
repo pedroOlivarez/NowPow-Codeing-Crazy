5535d56ade3194a557ec4c1a407e433e18d78958
Pedro Olivarez <pedro.olivarezjr@gmail.com>
2020-02-22 01:03:49 -0600

    longest increasing subsequence

---
+++ Challenges/JavaScript/LongestIncreasingSubsequence.js
@@ -0,0 +1,51 @@
+const input = [1, 3, 6, 7, 9, 4, 10, 5, 6];
+const expectedOutput = 6;
+
+function findLongestIncreasingSubsequence(nums) {
+   let max = 0;
+   if (!nums.length) return max;
+   const ourMap = new Map();
+   const formattedArray = nums.map((value, index) => {
+      const subSequence = nums.slice(index + 1);
+      const numGreaterAfter = subSequence.filter(ss => ss > value).length;
+      return {
+         index: index,
+         value: value,
+         numGreaterAfter: numGreaterAfter
+      };
+   });
+
+   formattedArray.forEach(value => {
+      let max = 0;
+      const mapValue = [value];
+      const appendableCandidates = formattedArray.filter(subValue => {
+         return subValue.value > value.value && subValue.index > value.index;
+      });
+      while (appendableCandidates.length) {
+         // This sorts the numbers that could potentially be appended first by how many numbers in the array occuring after that value
+         // are greater than that value and secondarily by how early that value appears in the array
+         appendableCandidates.sort((a, b) => {
+            if (a.numGreaterAfter > b.numGreaterAfter) return 1;
+            if (b.numGreaterAfter > a.numGreaterAfter) return -1;
+            if (a.index > b.index) return -1;
+            if (b.index > a.index) return 1;
+            return 0;
+         });
+         const bestCandidate = appendableCandidates.pop();
+         if (bestCandidate.index > mapValue[mapValue.length - 1].index && bestCandidate.value > mapValue[mapValue.length - 1].value) {
+            mapValue.push(bestCandidate);
+         }
+      }
+      ourMap.set(value.index, mapValue);
+   });
+   ourMap.forEach(value => {
+      if (value.length > max) max = value.length;
+   });
+
+   return max;
+}
+
+const myOutput = findLongestIncreasingSubsequence(input);
+
+console.log(`Expected Ouput is:  ${expectedOutput}`);
+console.log(`My output is:       ${myOutput}`);
